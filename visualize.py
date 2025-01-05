45b95bda08688630f6b0094ed909d85051f14758
Gaurav-Kapoor-07 <gkapoordelhi@gmail.com>
2025-01-05 13:29:59 +0100

    added landmarks and camera pose trajectory viz

--- visualize.py
+++ visualize.py
@@ -1,17 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def VisualizeKeypoints(img, axs, camera_pose, keypoints):
+def VisualizeKeypoints(img, axs, camera_pose, keypoints, poses_list, landmarks):
     """
     Visualize the keypoints and camera poses
     """
-    axs[0].cla()
-    axs[1].cla()
+    axs[0, 0].cla()
+    axs[0, 1].cla()
+    axs[1, 1].cla()
 
-    axs[1].set_xlim([-5, 200])
-    axs[1].set_ylim([-5, 5])
+    # axs[0, 1].set_xlim([-300, 300])
+    # axs[0, 1].set_ylim([-300, 300])
 
-    axs[0].imshow(img, cmap='gray')
+    axs[0, 0].imshow(img, cmap='gray')
 
     keypoints_filter = []
     for j in range(keypoints.shape[0]):
@@ -20,15 +21,22 @@ def VisualizeKeypoints(img, axs, camera_pose, keypoints):
 
     keypoints_filter = np.array(keypoints_filter)
 
-    axs[0].plot(keypoints_filter[:, 0], keypoints_filter[:, 1], 'o', color='green', markerfacecolor='none', linewidth=2, label = 'keypoints') 
-    axs[0].legend(loc='upper right')
+    axs[0, 0].plot(keypoints_filter[:, 0], keypoints_filter[:, 1], 'o', color='green', markerfacecolor='none', linewidth=2, label = 'keypoints') 
+    axs[0, 0].legend(loc='upper right')
 
-    axs[1].plot(camera_pose[0, 3], camera_pose[1, 3], 'bo', linewidth = 2, label = 'camera_poses')
-    axs[1].legend(loc='upper right')
+    for k in range(len(poses_list)):
+        axs[0, 1].plot(poses_list[k][0, 3], poses_list[k][1, 3], 'bo', linewidth = 2, label = 'camera_poses')
 
-    axs[1].set_xlabel('x')
-    axs[1].set_ylabel('y')
-    axs[1].set_title("Camera Poses")
+    axs[0, 1].set_xlabel('x')
+    axs[0, 1].set_ylabel('y')
+    axs[0, 1].set_title("Camera Poses")
+
+    axs[1, 1].plot(landmarks[:, 0], landmarks[:, 2]) 
+    axs[1, 1].set_xlabel('x')
+    axs[1, 1].set_ylabel('z')
+    axs[1, 1].set_title("Landmarks")
+
+    axs[1, 0].axis("off")
 
     plt.tight_layout()
 
