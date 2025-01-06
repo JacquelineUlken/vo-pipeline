5c7962114c1360f617a886d223800e8001496919
Jacqueline Ulken <ulken.jacqueline@gmail.com>
2025-01-06 12:24:00 +0100

    Some edits to the visualization

--- visualize.py
+++ visualize.py
@@ -4,6 +4,7 @@ from state import State
 import numpy as np
 import cv2
 
+
 class Visualization:
     def __init__(self, dataset: Dataset):
         self.dataset = dataset
@@ -12,11 +13,7 @@ class Visualization:
         self.number_of_candidates = []
         self.frames = []
 
-    def update(self, frame_idx, state: State, poses):
-        """
-        Update the visualization with the current state and poses.
-        """
-        # Displaying the image with the keypoints
+    def plot_current_frame(self, frame_idx, state: State):
         ax1 = self.axs[0, 0]
         ax1.cla()
 
@@ -28,58 +25,70 @@ class Visualization:
         ax1.imshow(image)
 
         keypoints = state.keypoints
-        candidates = state.candidate_keypoints
+        candidates = state.candidates
 
-        ax1.plot(candidates[:, 0], candidates[:, 1], 'x', color='green', markerfacecolor='none', linewidth=1, label="all candidates")
-        ax1.plot(keypoints[:, 0], keypoints[:, 1], 'o', color='red', markerfacecolor='none', linewidth=1, label="all keypoints")
-        ax1.legend(loc='upper right')
+        ax1.plot(candidates[:, 0], candidates[:, 1], "x", color="green", markerfacecolor="none", alpha=0.8, linewidth=1, label="all candidates")
+        ax1.plot(keypoints[:, 0], keypoints[:, 1], "o", color="red", markerfacecolor="none", alpha=0.8, linewidth=1, label="all keypoints")
+        ax1.legend(loc="lower right")
 
-        # Displaying local trajectory and landmarks
+    def plot_trajectory_and_landmarks(self, state: State, poses):
         ax2 = self.axs[0, 1]
         ax2.cla()
-        ax2.set_title("Local trajectory and landmarks")
-        ax2.set_xlabel('x')
-        ax2.set_ylabel('z')
-        ax2.set_aspect('equal')
+        ax2.set_title("Trajectory of the last 20 frames and landmarks")
+        ax2.set_xlabel("x")
+        ax2.set_ylabel("z")
+        ax2.set_aspect("equal")
 
         center = poses[-1, :, 3]
-
         landmarks = state.landmarks
-
         last_poses = poses[-20:] if len(poses) > 20 else poses
-        ax2.plot(landmarks[:, 0], landmarks[:, 2], 'o', color='red', markerfacecolor='none', linewidth=1, label="landmarks")
-        ax2.plot(last_poses[:, 0, 3], last_poses[:, 2, 3], color='blue', marker='.')
 
-        xlim = ax2.get_xlim()
-        ylim = ax2.get_ylim()
+        ax2.plot(landmarks[:, 0], landmarks[:, 2], "o", color="red", markerfacecolor="none", linewidth=1, label="Landmarks")
+        ax2.plot(last_poses[:, 0, 3], last_poses[:, 2, 3], color="blue", marker=".", label="Trajectory")
 
-        max_range = min(max(xlim[1] - xlim[0], ylim[1] - ylim[0]), 500)
+        if landmarks.shape[0] >= 200:
+            distances = np.linalg.norm(landmarks[:, :3] - center[:3], axis=1)
+            sorted_indices = np.argsort(distances)
+            visible_landmarks = landmarks[sorted_indices[:200]]
+        else:
+            visible_landmarks = landmarks
+
+        x_min = min(np.min(visible_landmarks[:, 0]), np.min(last_poses[:, 0, 3]))
+        x_max = max(np.max(visible_landmarks[:, 0]), np.max(last_poses[:, 0, 3]))
+        z_min = min(np.min(visible_landmarks[:, 2]), np.min(last_poses[:, 2, 3]))
+        z_max = max(np.max(visible_landmarks[:, 2]), np.max(last_poses[:, 2, 3]))
+
+        x_range = x_max - x_min
+        z_range = z_max - z_min
+        max_range = max(x_range, z_range)
 
         ax2.set_xlim(center[0] - max_range / 2, center[0] + max_range / 2)
         ax2.set_ylim(center[2] - max_range / 2, center[2] + max_range / 2)
+        ax2.legend(loc="lower right")
 
-        # Displaying number of keypoints and candidate keypoints
+    def plot_landmark_counts(self):
         ax3 = self.axs[1, 0]
         ax3.cla()
         ax3.set_title("Number of landmarks and candidate keypoints")
-        ax3.set_xlabel('Frame')
-        ax3.set_ylabel('Number of points')
+        ax3.set_xlabel("Frame")
+        ax3.set_ylabel("Number of points")
         ax3.grid(True)
 
         ax3.set_xlim(0, len(self.dataset))
-        ax3.set_ylim(0, 500)
+        ax3.set_ylim(0, 600)
 
-        ax3.plot(self.number_of_landmarks, color='red', label="Number of landmarks")
-        ax3.plot(self.number_of_candidates, color='green', label="Number of candidates")
+        ax3.plot(self.number_of_landmarks, color="red", linewidth=1, alpha=0.8, label="Number of landmarks")
+        ax3.plot(self.number_of_candidates, color="green", linewidth=1, alpha=0.8, label="Number of candidates")
+        ax3.legend(loc="lower right")
 
-        # Displaying global trajectory
+    def plot_global_trajectory(self, poses):
         ax4 = self.axs[1, 1]
         ax4.cla()
         ax4.set_title("Global trajectory")
-        ax4.set_xlabel('x')
-        ax4.set_ylabel('z')
-        ax4.set_aspect('equal')
-        ax4.plot(poses[:, 0, 3], poses[:, 2, 3], color='blue')
+        ax4.set_xlabel("x")
+        ax4.set_ylabel("z")
+        ax4.set_aspect("equal")
+        ax4.plot(poses[:, 0, 3], poses[:, 2, 3], color="blue", label="Trajectory")
 
         xlim = ax4.get_xlim()
         ylim = ax4.get_ylim()
@@ -90,25 +99,37 @@ class Visualization:
 
         ax4.set_xlim([x_center - max_range / 2, x_center + max_range / 2])
         ax4.set_ylim([y_center - max_range / 2, y_center + max_range / 2])
+        ax4.legend(loc="lower right")
+
+    def update(self, frame_idx, state: State, poses):
+        self.plot_current_frame(frame_idx, state)
+        self.plot_trajectory_and_landmarks(state, poses)
+        self.plot_landmark_counts()
+        self.plot_global_trajectory(poses)
 
         plt.tight_layout()
         plt.pause(0.01)
 
-        # Update the number of landmarks and candidates
         self.number_of_landmarks.append(len(state.landmarks))
-        self.number_of_candidates.append(len(state.candidate_keypoints))
+        self.number_of_candidates.append(len(state.candidates))
 
         self.fig.canvas.draw()
         frame = np.frombuffer(self.fig.canvas.tostring_rgb(), dtype=np.uint8)
         frame = frame.reshape(self.fig.canvas.get_width_height()[::-1] + (3,))
         self.frames.append(frame)
 
-    def save_video(self, filename='output.avi', fps=10):
+    def save_video(self, filename="videos/output.mp4", fps=10):
         """
-        Save the collected frames as a video.
+        Save the collected frames as an MP4 video with corrected colors.
         """
         height, width, _ = self.frames[0].shape
-        out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'XVID'), fps, (width, height))
+
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # MP4 codec
+
+        out = cv2.VideoWriter(filename, fourcc, fps, (width, height))
+
         for frame in self.frames:
-            out.write(frame)
+            frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            out.write(frame_bgr)
+
         out.release()
